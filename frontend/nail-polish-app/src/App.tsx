7df77935faf32ba5329ae5eaf4c6954ec8007943
GooliaJulia <juliadwyer93@gmail.com>
2025-02-24 16:50:50 +0100

    Create React TS app with Vite

---
+++ frontend/nail-polish-app/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { UploadImageScreen } from './screens/UploadImage'
+
+function App() {
+
+  return (
+    <>
+      <UploadImageScreen/>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/nail-polish-app/src/screens/UploadImage.tsx
@@ -0,0 +1,9 @@
+
+export function UploadImageScreen() {
+
+    return (
+        <>
+        Upload Image Screen
+        </>
+    )
+}
